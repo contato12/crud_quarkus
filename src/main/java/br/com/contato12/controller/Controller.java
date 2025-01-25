bf6765b93e4060c6b28dfcfbf68576529eb7f229
Marcondes Cunha Araujo <contato12araujo@google.com>
2025-01-25 19:46:14 -0300

    Inicio

---
+++ src/main/java/br/com/contato12/controller/Controller.java
@@ -0,0 +1,18 @@
+package br.com.contato12.controller;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/users")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class Controller {
+    @GET
+    public Response Test(){
+        return Response.ok("teste").build();
+    }
+}
